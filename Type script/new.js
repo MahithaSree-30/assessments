57ef4ad93abd3f1eaa384bb6e0fba638d1675666
MahithaSree-30 <239y1a05a2@ksrmce.ac.in>
2026-03-02 11:14:52 +0530

    Add files via upload

---
+++ Type script/new.js
@@ -0,0 +1,67 @@
+//lab1 and lab2
+// let num:number|string;
+// num=23456;
+// console.log(num);
+// num="hi world";
+// console.log(num);
+//lab5 task 1
+// function greetUser(name:string){
+//     return("I am "+name)
+// }
+// console.log(greetUser("Mahitha"))
+//lab5 task 2
+//function add(a:number,b:number){
+//     return(a*b)
+// }
+// function multiply(a:number,b:number){
+//     return(a+b)
+// }
+// console.log(add(44,55))
+// console.log(multiply(33,44))
+//lab6 task 1
+// function calculateSquare(num:number):number{
+//     return num*num
+// }
+// console.log(calculateSquare(9))
+//lab6 task2
+// function isEven(num:number):boolean{
+//     if (num%2==0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// console.log(isEven(45678))
+// lab7 task 1
+// function greetUser(name:string, title:string){
+//     if(title==undefined){
+//         return("I am "+name)
+//     }
+//     else{
+//         return("I am "+title+" "+name)
+//     }
+// }
+// console.log(greetUser("Mahitha"))
+//lab7 task2
+// const sum=(num1:number,num2:number)=>{
+//     console.log(num1+num2)
+// }
+// sum(33,88)
+// sum(55,44)
+//lab 8 task 1
+// type point={
+//     x:number,
+//     y:number
+// }
+// function printPoint(pt:point){
+//     console.log(pt.x)
+//     console.log(pt.y)
+// }
+// printPoint({x:8000,y:459})
+//lab9 task 1
+function move(direction) {
+    console.log("Moving ".concat(direction));
+}
+move("up");
+move("left");
