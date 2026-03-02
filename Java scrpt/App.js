ff78792252c33e31484798dfd3ef04025417b46f
MahithaSree-30 <239y1a05a2@ksrmce.ac.in>
2026-03-02 11:00:45 +0530

    Update App.js

--- Java scrpt/App.js
+++ Java scrpt/App.js
@@ -1,30 +1,30 @@
-// import {BrowserRouter,Routes,Route} from "react-router-dom";
-// import './App.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import './App.css';
 
-// import Navbar from './Navbar/Navbar';
-// import Home from './Home/Home';
-// import About from './About/About';
-// import Staff from './Staff/Staff';
-// import Contact from './Contact/Contact';
-// import Menu from './Menu/Menu';
-// import UsersTable from "./Axios";
+import Navbar from './Navbar/Navbar';
+import Home from './Home/Home';
+import About from './About/About';
+import Staff from './Staff/Staff';
+import Contact from './Contact/Contact';
+import Menu from './Menu/Menu';
+import UsersTable from "./Axios";
 
-// function App() {
-//   return (
-    // <BrowserRouter>
-    //   <Navbar />
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
 
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/menu" element={<Menu />} />
-    //     <Route path="/staff" element={<Staff />} />
-    //     <Route path="/contact" element={<Contact />} />
-    //   </Routes>
-    // </BrowserRouter>
-//     <UsersTable/>
-//   );
-// }
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/staff" element={<Staff />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+    <UsersTable/>
+  );
+}
 
-// export default App;
+export default App;
 
