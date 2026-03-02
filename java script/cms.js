5bc220a1ce6123a9fb4890d91f288943a2928fd0
MahithaSree-30 <239y1a05a2@ksrmce.ac.in>
2026-03-02 11:47:17 +0530

    Create cms.js

---
+++ java script/cms.js
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CMS</title>
+    <link rel="stylesheet" href="cms.css">
+</head>
+<body>
+    <nav>
+        <div class="main">
+        <img src="https://www.shutterstock.com/image-vector/clinic-logo-butterfly-health-600nw-2553935777.jpg">
+        <div class="doc">
+        <center>
+        <a href="home.html">Home</a>
+        <a href="Doctors.html">Doctors</a>
+        <a href="Appointments.html">Appointments</a>
+        <a href="About.html">About</a>
+        <a href="Contact.html">Contact</a>
+        <a href="log in.html">log in</a>
+        <a href="sign in.html">sign in</a><br><br><br>
+        </center>
+        </div>
+        </div>
+    </nav>
+    <div style="background-color:skyblue">
+        <center><h2 style="font-size:550%"></h2>
+        <h2 style="background-color: pink;">Welcome to clinic management system</h2>
+        <div class="doctor-card">
+            <img src="https://bouve.northeastern.edu/wp-content/uploads/2023/06/doctor-of-health-science-vs-doctor-of-medical-science-whats-the-difference-northeastern-graduate.webp">
+            <img src="https://meridianhospitals.in/wp-content/uploads/2024/05/Dr-Abdul-Rahman.png">
+            <img src="https://images.ctfassets.net/4mpdf15r5lc4/6sbpwej7TDvaCVbuOVCaiE/a0abb620c7a8d5480366a09401a89b85/Hospital_doctor.webp">
+            <img src="https://desunwebsite-assets.s3.ap-south-1.amazonaws.com/desunhospital-com/wp-content/uploads/2024/12/05224129/Dr-Sreya-Mallick.jpg">
+            <img src="https://snibbs.co/cdn/shop/articles/What_are_the_Challenges_of_Being_a_Doctor.jpg?v=1684314843">
+           <h2 style="background-color: pink;">Information</h2>
+            <h3>Our clinic is dedicated to providing high-quality, compassionate healthcare to patients of all ages. We focus on accurate diagnosis, effective treatment, and personalized care in a comfortable and friendly environment.</h3>
+        </div></center>
+    </div>
+
+</body>
+<script src="csm.js"></script>
+</html>
