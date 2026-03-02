e8cb03f673d9724b1263977cd5f0f6cc04bfd393
MahithaSree-30 <239y1a05a2@ksrmce.ac.in>
2026-03-02 09:12:56 +0530

    Add files via upload

---
+++ Java scrpt/App.js
@@ -0,0 +1,30 @@
+// import {BrowserRouter,Routes,Route} from "react-router-dom";
+// import './App.css';
+
+// import Navbar from './Navbar/Navbar';
+// import Home from './Home/Home';
+// import About from './About/About';
+// import Staff from './Staff/Staff';
+// import Contact from './Contact/Contact';
+// import Menu from './Menu/Menu';
+// import UsersTable from "./Axios";
+
+// function App() {
+//   return (
+    // <BrowserRouter>
+    //   <Navbar />
+
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/about" element={<About />} />
+    //     <Route path="/menu" element={<Menu />} />
+    //     <Route path="/staff" element={<Staff />} />
+    //     <Route path="/contact" element={<Contact />} />
+    //   </Routes>
+    // </BrowserRouter>
+//     <UsersTable/>
+//   );
+// }
+
+// export default App;
+
